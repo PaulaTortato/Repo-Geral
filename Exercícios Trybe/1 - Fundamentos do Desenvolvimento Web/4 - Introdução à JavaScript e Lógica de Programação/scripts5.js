fde627c98ef42f8c1d538e169143db1ee87ab7e5
Paula <paulacwtortato@gmail.com>
2021-10-15 18:48:38 -0300

    Exercicios da aula 4.3 terminados

--- Exercícios Trybe/1 - Fundamentos do Desenvolvimento Web/4 - Introdução à JavaScript e Lógica de Programação/scripts5.js
+++ Exercícios Trybe/1 - Fundamentos do Desenvolvimento Web/4 - Introdução à JavaScript e Lógica de Programação/scripts5.js
@@ -1,4 +1,4 @@
-let n = 5;
+let n = 7;
 
 for (let index = 1; index <= n; index++) {
   console.log('*'.repeat(n));
@@ -18,6 +18,23 @@ for (let i = 1; i <= n; i++) {
 
 console.log('--------------------');
 
-for (let i = 1; i <= n; i++) {
-  
+for (let i = 1; i <= n; i += 2) {
+  console.log(' '.repeat((n-i) / 2) + '*'.repeat(i));
 }
+
+console.log('--------------------');
+
+let number = 11;
+let ePrimo;
+
+for (let i = 2; i < number; i++) {
+  if (number % i == 0) {
+    ePrimo = 'O número ' + number + ' não é primo.';
+    break;
+  }
+  else {
+    ePrimo = 'O número ' + number + ' é primo.';
+  };
+}
+
+console.log(ePrimo);
